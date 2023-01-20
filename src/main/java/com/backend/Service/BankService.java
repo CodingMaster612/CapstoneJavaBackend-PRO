beaa30c1a5d5d7950d7dc317d210483f5d5ecca7
CodingMaster612 <105937944+CodingMaster612@users.noreply.github.com>
2023-01-20 13:06:28 -0600

    added bank signin

--- src/main/java/com/backend/Service/BankService.java
+++ src/main/java/com/backend/Service/BankService.java
@@ -1,5 +1,28 @@
 package com.backend.Service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.backend.Entity.Bank;
+import com.backend.Entity.User;
+import com.backend.Repo.BankRepo;
+import com.backend.Repo.UserRepo;
+@Service
 public class BankService {
+	 
+	
+	@Autowired
+	BankRepo bankRepo;
+	
+	
+	public Bank signIn(Bank bank) {
+	        
+	       
+	        return bankRepo.signIn(bank.getEmail(), bank.getPassword());
+
+	       
+	    }
+
+
 
 }
